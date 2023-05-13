f76c921d94a821545ce9cca09000f1eddc8105b2
adminRV <dremerz.product@gmail.com>
2023-05-13 10:15:12 +0530

    rv

---
+++ src/rv/pages/add/std-atd-dep.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import Searchbar from '../../components/searchbar';
+
+export default function StdAtdDep() {
+  return (
+    <Searchbar data="Ford"  ></Searchbar>
+  )
+}
